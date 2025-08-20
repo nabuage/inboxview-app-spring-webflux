a5c297b46dd7c8ad47aa92438c4d8ab87b86cbfe
George <nabuage@outlook.com>
2025-08-20 14:18:27 -0500

    Disable testcontainers from test.

--- src/test/java/org/inboxview/app/TestInboxviewApplication.java
+++ src/test/java/org/inboxview/app/TestInboxviewApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestInboxviewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(InboxviewApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(InboxviewApplication::main).run(args);
 	}
 
 }

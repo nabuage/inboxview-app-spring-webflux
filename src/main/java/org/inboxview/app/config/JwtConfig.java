6639382e6ee7c66568f5278ba22abe185d6556f7
George <nabuage@outlook.com>
2025-08-20 11:45:46 -0500

    Load the keys from the host environment.

--- src/main/java/org/inboxview/app/config/JwtConfig.java
+++ src/main/java/org/inboxview/app/config/JwtConfig.java
@@ -1,11 +1,19 @@
 package org.inboxview.app.config;
 
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.time.Duration;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -23,17 +31,23 @@ import lombok.Setter;
 @Configuration
 @Setter
 @Getter
-@ConfigurationProperties(prefix = "jwt")
 public class JwtConfig {
+    private final String ALGORITHM = "RSA";
     private RSAPrivateKey privateKey;
     private RSAPublicKey publicKey;
+
+    @Value("${jwt.public-key}")
+    private String publicKeyString;
+    @Value("${jwt.private-key}")
+    private String privateKetString;
+    @Value("${jwt.access-token-ttl}")
     private Duration accessTokenTtl;
 
     @Bean
     public JwtEncoder jwtEncoder() {
         final var jwk = new RSAKey
-            .Builder(publicKey)
-            .privateKey(privateKey)
+            .Builder(getPublicKey())
+            .privateKey(getPrivateKey())
             .build();
 
         return new NimbusJwtEncoder(
@@ -43,7 +57,7 @@ public class JwtConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+        return NimbusJwtDecoder.withPublicKey(getPublicKey()).build();
     }
 
     @Bean
@@ -53,5 +67,48 @@ public class JwtConfig {
         final JwtDecoder jwtDecoder
     ) {
         return new JwtService(appName, accessTokenTtl, jwtEncoder, jwtDecoder);
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        if (privateKey == null) {            
+            try {
+                PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(
+                    Base64.getDecoder()
+                    .decode(
+                        privateKetString.getBytes(StandardCharsets.UTF_8)
+                    )
+                );
+
+                KeyFactory factory;
+                factory = KeyFactory.getInstance(ALGORITHM);
+
+                return (RSAPrivateKey) factory.generatePrivate(keySpec);
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            }
+        }
+
+        return privateKey;
+  }
+
+    public RSAPublicKey getPublicKey() {
+        if (publicKey == null) {            
+            try {
+                byte[] data = Base64.getDecoder()
+                .decode(
+                    publicKeyString.getBytes(StandardCharsets.UTF_8)
+                );
+
+                X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
+                KeyFactory factory;
+
+                factory = KeyFactory.getInstance(ALGORITHM);
+
+                publicKey = (RSAPublicKey) factory.generatePublic(spec);
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            }
+        }
+
+        return publicKey;
+        
     }
 }
